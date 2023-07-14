ea801ff2f851e232ea55addf8b726c45018c8efe
Your Name <you@example.com>
2023-07-14 03:46:40 +0000

    commit message

---
+++ src/components/ReadMore/styledComponents.js
@@ -0,0 +1,12 @@
+const TopContainer = styled.div``
+const Heading = styled.h1`
+  font-family: Roboto;
+`
+const Text = styled.p``
+const BottomContainer = styled.div`
+  width: 500px;
+`
+const Image = styled.img`
+  height: 300px;
+`
+const Button = styled.button``
